41bde16cbc155dbb1e9517f1f8cdbd46a1bfeff1
Love-Chukwuegbo <cchinweokenwa@gmail.com>
2025-06-09 17:25:19 +0100

    added arithemetic operations.py.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,18 @@
-def perform_operation(num1,num2, operation=""):
+def perform_operation(num1,num2, operation):
     if operation:   
         match operation:
             case 'add':
-                Result = num1 + num2
+                result = num1 + num2
             case 'subtract':
-                Result = num1 - num2
+                result = num1 - num2
             case 'multiply':
-                Result = num1 * num2
+                result = num1 * num2
             case 'divide':
                 if num2:
-                    Result = num1 / num2
+                    result = num1 / num2
                 else:
-                    Result= "Division by zero is n"
-        return Result
+                    result= "Unpermitted"
+        return result
     else:
         print("please add an operation")
 perform_operation(5.0,2.0, 'multiply')
