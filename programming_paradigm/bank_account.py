8692983630ce799111ebd4925dad03180c0bf66c
Love-Chukwuegbo <cchinweokenwa@gmail.com>
2025-07-13 12:44:59 +0100

    added programming paradigms

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount():
+    def __init__(self,bal =0):
+        self.account_balance = bal
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def  withdraw(self,amount):
+        fundSufficient =  self.account_balance >= amount
+        if fundSufficient:
+            self.account_balance -= amount
+            return fundSufficient
+        else:
+            return fundSufficient
+    def display_balance(self):
+        print('Dear customer your baalance is {}' . format(self.account_balance))
