2b6d97c29fe8656e3469b09b7556ffa937b11494
Love-Chukwuegbo <cchinweokenwa@gmail.com>
2025-07-15 00:00:07 +0100

    Editted the library mgt

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -7,24 +7,25 @@ class Book:
 book1= Book("Brave New World", "Aldous Huxley")
 
 class Library():
-    _books=[]
+    def __init__(self):
+        self._books=[]
     
     def add_book(self,book_obj):
-        Library._books.append(book_obj)
-        for book in Library._books:
+        self._books.append(book_obj)
+        for book in self._books:
         
             print("{} by {}".format(book.title, book.author))
     def check_out_book(self,title):
-        for book in Library._books:
+        for book in self._books:
             if book.title == title:
                 book._is_checked_out= True
                 break
     def return_book(self,title):
-        for book in Library._books:
+        for book in self._books:
             if book.title == title:
                 book._is_checked_out = False
                 break
     def list_available_books(self):
-        for book in Library._books:
+        for book in self._books:
             if book._is_checked_out == False:
                 print("{} by {}".format(book.title, book.author))
